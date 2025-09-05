5c4ea24cc85210694195d1517c5d41c158486c79
Mahak Bansal <mahakbansal82@gmail.com>
2025-09-05 19:12:40 +0530

    All changes

--- Component/InternshipExperience.jsx
+++ Component/InternshipExperience.jsx
@@ -3,9 +3,9 @@ import "./InternshipExperience.css"; // Import the CSS file
 
 const internships = [
   {
-    company: "Central Raiway of Information System,Delhi",
+    company: "Ducat School of Ai,Delhi",
     role: "FullStack Developer Intern",
-    duration: "Aug 2024 - Nov 2024",
+    duration: "feb 2025 - sep 2025",
     description:
       "Contributed to the Signal Maintenance and Management System project, developing and optimizing full-stack features using Angular, Node.js and postgreSql. Focused on building responsive UI dashboards, integrating backend APIs, and improving data flow for real-time signal tracking. Gained hands-on experience in version control, RESTful APIs, and agile development practices.",
   },
